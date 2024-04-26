b0fd6dc262b7ef37985a39b5eda2e8650a5e2796
EduardoPetrini <eduardo.petrini@outlook.com>
2024-04-26 11:23:14 -0300

    chore: streaming by events

--- event.js
+++ event.js
@@ -1,38 +1,12 @@
 const http = require('http');
-const snowflake = require('snowflake-sdk');
 const EventEmitter = require('events');
-const dotenv = require('dotenv');
+const SnowflakeConnector = require('./SnowflakeConnector');
+
 const { log } = console;
 
-dotenv.config();
-
-const credentials = {
-  account: process.env.SF_ACCOUNT,
-  database: process.env.SF_DATABASE,
-  warehouse: process.env.SF_WAREHOUSE,
-  username: process.env.SF_USERNAME,
-  password: process.env.SF_PASSWORD,
-};
-
-const sqlText = process.env.SQL_TEXT;
-
 const start = async () => {
-  log('creating connection');
-  const connection = snowflake.createConnection(credentials);
-  log('connecting');
-  await new Promise((resolve, reject) => connection.connect((err, conn) => (err ? reject(err) : resolve(conn))));
-
-  log('getting statement');
-  const statement = await new Promise((resolve, reject) => {
-    connection.execute({
-      sqlText,
-      streamResult: true,
-      complete: (err, stmt) => (err ? reject(err) : resolve(stmt)),
-    });
-  });
-
-  log('getting stream');
-  const dataStream = statement?.streamRows();
+  const sfInstance = await SnowflakeConnector.getInstance();
+  const dataStream = await sfInstance.getStreamData();
 
   const event = new EventEmitter();
 
